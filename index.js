dbfe4c30c35edd0dbedeb7cecdfb2b4a2c08a892
Annemarie <a.lucernoni0@gmail.com>
2022-04-26 19:36:10 -0700

    commit tree c1d360ee7d85e91e5d4dd232c17274e358ac4d6d

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+
+var customerName = 'bob'
+
+var upperCaseCustomerName = () => customerName = customerName.toUpperCase()
+
+// function upperCaseCustomerName(name) {
+//     customerName = customerName.toUpperCase();
+// }
+
+var setBestCustomer = () => bestCustomer = 'not bob'
+
+var overwriteBestCustomer = () => bestCustomer = 'maybe bob'
+
+const leastFavoriteCustomer = 'bingo bango bongo'
+
+var changeLeastFavoriteCustomer = () => leastFavoriteCustomer = 'bazinga!'
+
